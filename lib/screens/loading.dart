d480eb14696979cb5c5992e480c1788c7dba1e62
jihhye1116 <simjihye@simjihyeui-MacBookAir.local>
2024-10-28 10:19:29 +0900

    HTTP를 사용한 에러처리

--- lib/screens/loading.dart
+++ lib/screens/loading.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart';
 
 class Loading extends StatefulWidget {
   const Loading({Key? key}) : super(key: key);
@@ -13,13 +17,45 @@ class _LoadingState extends State<Loading> {
   void initState() {
     super.initState();
     getLocation();
+    fetchData();
   }
 
   void getLocation() async {
-    LocationPermission permission = await Geolocator.requestPermission();
-    Position position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.high);
-    print(position);
+    try {
+      LocationPermission permission = await Geolocator.requestPermission();
+      Position position = await Geolocator.getCurrentPosition();
+      flutterToast(position.toString());
+      print("position");
+      print(position);
+    } catch (e) {
+      flutterToast('There was a problem with the internet connection');
+    }
+  }
+
+  void fetchData() async {
+    try {
+      Response response = await get(Uri.parse(
+          'https://samples.openweathermap.org/data/2.5/weather?q=London&appid=b1b15e88fa797225412429c1c50c122a1'));
+      if (response.statusCode == 200) {
+        String jsonData = response.body;
+        var myWeather = jsonDecode(jsonData)['weather'][0]['description'];
+        flutterToast(myWeather.toString());
+        //print(myWeather.toString());
+
+        var myWind = jsonDecode(jsonData)['wind']['speed'];
+        flutterToast(myWind.toString());
+        //print(myWind);
+
+        var myId = jsonDecode(jsonData)['id'];
+        flutterToast(myId.toString());
+        //print(myId);
+      }
+      print(response.statusCode);
+    } catch (e) {
+      flutterToast('There was a problem with the internet connection');
+    }
+    /*return Future.delayed(
+        Duration(seconds: 3), () => throw Exception('response failed'));*/
   }
 
   @override
@@ -29,21 +65,26 @@ class _LoadingState extends State<Loading> {
         child: ElevatedButton(
           onPressed: () {
             getLocation();
+            fetchData();
           },
-          child: Text(
-            'Get my location',
-            style: TextStyle(
-              color: Colors.white,
-            ),
-          ),
           style: ElevatedButton.styleFrom(
             backgroundColor: Colors.blue,
             shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(4),
+              borderRadius: BorderRadius.circular(2),
+            ),
+          ),
+          child: const Text(
+            'Get my location',
+            style: TextStyle(
+              color: Colors.white,
             ),
           ),
         ),
       ),
     );
   }
+}
+
+void flutterToast(String msg) {
+  Fluttertoast.showToast(msg: msg, toastLength: Toast.LENGTH_SHORT);
 }
